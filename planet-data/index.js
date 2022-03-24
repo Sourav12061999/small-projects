bd1fb3dc81d750fb479a6950d9720879b30faadf
Sourav Das <sdunity12061999@gmail.com>
2022-03-24 19:26:27 +0530

    csv parse pipe added to fs stream

--- planet-data/index.js
+++ planet-data/index.js
@@ -3,6 +3,12 @@ const fs = require("fs");
 
 const results = [];
 fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
     results.push(data);
   })
