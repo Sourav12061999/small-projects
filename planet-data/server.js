01ddd8875225c7ce9ca4c9d1c3f9368b6bea65b7
Sourav Das <sdunity12061999@gmail.com>
2022-03-25 13:53:34 +0530

    post method added

--- planet-data/server.js
+++ planet-data/server.js
@@ -1,11 +1,18 @@
 const http = require("http");
 
+let arr = [];
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     "Content-Type": "text/plain",
   });
   if (req.url === "/hello") {
     res.write("Hello World");
+  } else if (req.url === "/post" && req.method === "POST") {
+    req.on("data", (data) => {
+      const postData = data.toString();
+      console.log(postData);
+      arr.push(postData);
+    });
   } else {
     res.write("Don't know this url");
   }
