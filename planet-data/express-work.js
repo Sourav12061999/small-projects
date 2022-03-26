2d158afeba13003cee3ca73d8337710e8fdc22b4
Sourav Das <sdunity12061999@gmail.com>
2022-03-26 09:41:51 +0530

    static html page serving added

--- planet-data/express-work.js
+++ planet-data/express-work.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use("/site", express.static(__dirname, "public"));
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "codebash2.png"));
 });
