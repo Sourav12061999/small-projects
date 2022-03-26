246c54a7d80377bd847db5369b76fb5c02f9d128
Sourav Das <sdunity12061999@gmail.com>
2022-03-26 09:38:49 +0530

    file sending learned with express

---
+++ planet-data/express-work.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use("/site", express.static(__dirname, "public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "codebash2.png"));
+});
+app.listen(8080, () => {
+  console.log("Server started");
+});
